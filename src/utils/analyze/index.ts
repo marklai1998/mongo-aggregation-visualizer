ec63425e9e3b5e368f3171ab3fba04adea7803dc
marklai1998 <marklai1998@pm.me>
2025-04-06 14:16:12 -0400

    fix: empty pipeline

--- src/utils/analyze/index.ts
+++ src/utils/analyze/index.ts
@@ -37,29 +37,12 @@ export type StageAnalyzer<S extends Stage> = (arg: {
   stage: S;
 }) => void;
 
-const createCollection = (res: AnalysisResult, name: string) => {
-  if (res.collections[name]) return;
-  const id = `${name}._id`;
-
-  res.collections[name] = {
-    fields: {
-      _id: {
-        id,
-        type: FieldType.DEFAULT,
-        color: getColor(id),
-      },
-    },
-  };
-};
-
 export const isFieldResult = (v: Fields | FieldResult): v is FieldResult =>
   'type' in v;
 
 export const analyze = (aggregation: Aggregation) =>
   aggregation.reduce<AnalysisResult>(
     (state, stage) => {
-      createCollection(state, DEFAULT_COLLECTION);
-
       const arg = { state, collection: DEFAULT_COLLECTION };
 
       if ('$addFields' in stage) analyzeAddField({ ...arg, stage });
@@ -68,7 +51,17 @@ export const analyze = (aggregation: Aggregation) =>
       return state;
     },
     {
-      collections: {},
+      collections: {
+        [DEFAULT_COLLECTION]: {
+          fields: {
+            _id: {
+              id: `${DEFAULT_COLLECTION}._id`,
+              type: FieldType.DEFAULT,
+              color: getColor(`${DEFAULT_COLLECTION}._id`),
+            },
+          },
+        },
+      },
       result: {
         _id: {
           id: `${DEFAULT_COLLECTION}._id`,
