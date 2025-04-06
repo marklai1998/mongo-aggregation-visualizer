40d40e43c936bb9ebe08bf7d3da8100fd8d54c82
marklai1998 <marklai1998@pm.me>
2025-04-06 13:18:20 -0400

    fix: key length

--- src/types/aggregation.ts
+++ src/types/aggregation.ts
@@ -1,11 +1,14 @@
 import { type Static, Type } from '@sinclair/typebox';
 
 export const unset = Type.Object({
-  $unset: Type.Union([Type.String(), Type.Array(Type.String())]),
+  $unset: Type.Union([
+    Type.String({ minLength: 1 }),
+    Type.Array(Type.String({ minLength: 1 })),
+  ]),
 });
 
 export const addField = Type.Object({
-  $addFields: Type.Mapped(Type.String(), () => Type.Unknown()),
+  $addFields: Type.Mapped(Type.String({ minLength: 1 }), () => Type.Unknown()),
 });
 
 export const stage = Type.Union([addField, unset]);
